ce74dd6a649f9381d4e10f2b31c3b8f5c0023aa5
Tomasz Szczerba <t.szczerba@hotmail.com>
2017-09-17 17:41:45 +0200

    1st try

---
+++ packages/addons/tools/plugin.program.hyperion.screenshot/package.mk
@@ -0,0 +1,34 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="plugin.program.hyperion.screenshot"
+PKG_VERSION="cc85d32"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/LightberryEu/plugin.program.hyperion.screenshot"
+PKG_URL="https://github.com/LightberryEu/plugin.program.hyperion.screenshot/archive/$PKG_VERSION.tar.gz"
+PKG_SECTION="tools"
+PKG_SHORTDESC="Hyperion screenshot tester"
+PKG_LONGDESC="Hyperion screenshot tester"
+PKG_AUTORECONF="yes"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Hyperion grabber screenshot"
+PKG_ADDON_TYPE="xbmc.python.script"
+PKG_MAINTAINER="Lightberry (lightberry@lightberry.eu)"
